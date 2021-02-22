7f1b023e396b8252ff716b9ee98205d38a95a6f5
Akshar Patel <akshar.patel.47@gmail.com>
2021-02-21 22:53:23 -0700

    feat: add an spa pipeline

---
+++ vars/useSpaPipeline.groovy
@@ -0,0 +1,57 @@
+def call(Map config) {
+    pipeline {
+        agent any
+        stages {
+            stage('Precheck') {
+                steps {
+                    sh 'docker -v'
+                    script {
+                        docker.image("node:lts-alpine").inside {
+                            sh 'node --version'
+                        }
+                    }
+                }
+            }
+            
+            stage('Test') {
+                steps {
+                    script {
+                        docker.image("node:lts-alpine").inside {
+                            sh 'npm run test'
+                        }    
+                    }
+                }
+            }
+            
+            stage('Release Build') {
+                steps {
+                    script {
+                        docker.image("node:lts-alpine").inside {
+                            sh "npm run build:prod"
+                            writeFile encoding: 'UTF8', file: 'Dockerfile', text: """FROM nginx:1.19-alpine
+COPY dist /usr/share/nginx/html
+"""
+                        }
+                    }
+                }
+            }
+            
+            stage('Build & Push Docker Image') {
+                when {
+                    branch 'master'
+                }
+                steps {
+                    script {
+                        docker.withRegistry('', 'docker-hub-cred') {
+                            def imageName = "aksharpatel47/${config.dockerImageName ? config.dockerImageName : currentBuild.projectName}:${env.BUILD_NUMBER}";
+                            echo "Building image ${imageName}..."
+                            def build = docker.build(imageName)
+                            build.push("${env.BUILD_NUMBER}")
+                            build.push("latest")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
