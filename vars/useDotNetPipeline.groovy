ae2b025d91b316fb374ab647ec6d6d14c6cdcc0f
Akshar Patel <akshar.patel.47@gmail.com>
2021-03-25 17:59:48 -0700

    update dotnet pipeline

--- vars/useDotNetPipeline.groovy
+++ vars/useDotNetPipeline.groovy
@@ -9,7 +9,7 @@ def call(Map config) {
                 steps {
                     sh 'docker -v'
                     script {
-                        docker.image("mcr.microsoft.com/dotnet/sdk:${config.dotnetVersion}").inside {
+                        docker.image("mcr.microsoft.com/dotnet/sdk:${config.dotnetVersion}-alpine").inside {
                             sh 'dotnet --info'
                         }
                     }
@@ -19,7 +19,7 @@ def call(Map config) {
             stage('Test') {
                 steps {
                     script {
-                        docker.image("mcr.microsoft.com/dotnet/sdk:${config.dotnetVersion}").inside {
+                        docker.image("mcr.microsoft.com/dotnet/sdk:${config.dotnetVersion}-alpine").inside {
                             sh 'dotnet test'
                         }
                     }
@@ -29,9 +29,9 @@ def call(Map config) {
             stage('Release Build') {
                 steps {
                     script {
-                        docker.image("mcr.microsoft.com/dotnet/sdk:${config.dotnetVersion}").inside {
+                        docker.image("mcr.microsoft.com/dotnet/sdk:${config.dotnetVersion}-alpine").inside {
                             sh "dotnet build ${config.projectName}/${config.projectName}.csproj --configuration Release -o output -r linux-x64"
-                            writeFile encoding: 'UTF8', file: 'Dockerfile', text: """FROM mcr.microsoft.com/dotnet/runtime:${config.dotnetVersion}
+                            writeFile encoding: 'UTF8', file: 'Dockerfile', text: """FROM mcr.microsoft.com/dotnet/runtime:${config.dotnetVersion}-alpine
 COPY output /app
 WORKDIR /app
 ENTRYPOINT ["dotnet", "${config.projectName}.dll"]"""
@@ -42,12 +42,19 @@ ENTRYPOINT ["dotnet", "${config.projectName}.dll"]"""
             
             stage('Build & Push Docker Image') {
                 when {
-                    branch 'master'
+                    anyOf {
+                        branch 'master';
+                        branch 'develop';
+                        branch 'Release-\\w+';
+                        branch 'HotFix-\\w+'
+                    }
+                    
                 }
                 steps {
                     script {
                         docker.withRegistry('', 'docker-hub-cred') {
-                            def imageName = "aksharpatel47/${config.dockerImageName ? config.dockerImageName : currentBuild.projectName}:${env.BUILD_NUMBER}"
+                            // def imageName = "aksharpatel47/${config.dockerImageName ? config.dockerImageName : currentBuild.projectName}:${env.BUILD_NUMBER}"
+                            def imageName = "aksharpatel47/${getImageTag(config.dockerImageName, env.BRANCH_NAME, env.BUILD_NUMBER)}"
                             echo "Building image ${imageName}..."
                             def build = docker.build(imageName)
                             build.push("${env.BUILD_NUMBER}")
@@ -58,4 +65,20 @@ ENTRYPOINT ["dotnet", "${config.projectName}.dll"]"""
             }
         }
     }
+}
+
+def getImageTag(imageName, branchName, buildNumber) {
+    def imageTag = imageName
+    if (branchName.equals('master')) {
+        def version = sh(returnStdout: true, script: "git tag --contains").trim()
+        imageTag += "-prod:${version}"
+    }
+    else if (branchName.equals('develop')) {
+        imageTag += '-dev:${buildNumber}'
+    } else if (branchName.startsWith('Release') || branchName.startsWith('HotFix')) {
+        def version = branchName.split("/")[1]
+        imageTag += "-qa-${version}:${buildNumber}"
+    }
+
+    return imageTag
 }
